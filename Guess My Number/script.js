6b3b60dbbf130d675daf8f2bc538db7356e33e4c
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-18 15:37:32 +0530

    JS
    Added JS

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,52 @@
 'use strict';
+const number = document.querySelector('.number');
+const input = document.querySelector('.guess');
+const checkBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
+const again = document.querySelector('.again');
+
+let gameOver = false;
+
+let randomNumber = Math.floor(Math.random() * 20 + 1);
+number.innerText = randomNumber;
+
+checkBtn.addEventListener('click', () => {
+  if (!gameOver && input.value) {
+    let currentScore = score.innerText;
+
+    if (randomNumber == input.value) {
+      highscore.innerText = currentScore;
+      document.body.style.backgroundColor = ' #60b347';
+      gameOver = true;
+      message.innerText = '🎉 Correct Number!';
+      number.innerText = randomNumber;
+      number.style.width = '30rem';
+    } else {
+      score.innerText = currentScore - 1;
+
+      //   if (Number(input.value) < Number(number.innerText))
+      //     message.innerText = '📉 Too low!';
+      //   else message.innerText = '📈 Too high!';
+
+      message.innerText =
+        Number(input.value) > Number(number.innerText)
+          ? '📈 Too high!'
+          : '📉 Too low!';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score.innerText = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  score.textContent = score;
+  number.textContent = '?';
+  input.value = '';
+
+  document.body.style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
