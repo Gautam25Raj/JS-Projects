3b762208f2739919a9e696feb759f1250034c0eb
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-20 15:29:05 +0530

    Modal Window Js
    Final code of javaScript uploaded

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const btnsOpen = document.querySelectorAll('.show-modal');
+const btnClose = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnsOpen.forEach(btnOpen => {
+  btnOpen.addEventListener('click', openModal);
+});
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+window.addEventListener('keydown', e => {
+  if (e.key == 'Escape') closeModal();
+});
